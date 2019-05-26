50cef19d098c57e03498977903ef001ba1da59cd
José Joaquim <jjaneto@MacBook-Pro-de-Jose.local>
2019-05-26 12:24:30 -0300

    Implementing interference distance matrix

--- Model.cpp
+++ Model.cpp
@@ -24,9 +24,15 @@ Model::Model(char *file) {
 
   //TODO: change the dummy variables to its respective correct names
 
-  double aux1, aux2, aux3, aux4, aux5, aux6, aux7, aux8, aux9, aux10;
+  double aux1;
   fscanf(file1, "%lf", &aux1);
-  fscanf(file1, "%lf %lf %lf %lf %lf %lf %lf %lf %lf", &aux2, &aux3, &aux4, &aux5, &aux6, &aux7, &aux8, &aux9, &aux10);
+  fscanf(file1, "%d %lf %lf %lf %lf %lf %lf %lf %lf", &nConnections, &time, &alfa, &noise, &powerSender, &aux1, &aux1, &aux1, &aux1);
+
+  /*  //TODO: Check why this is necessary
+  if (noise != 0) {
+    noise = convertDBMToMW(noise);
+  }
+   */
 
   //interferenceMatrix.assign();
   //distanceMatrix.assing();
@@ -42,6 +48,9 @@ Model::Model(char *file) {
       fscanf(file1, "%lf", &SINR[i][j]);
     }
   }
+
+  //TODO: Check why this is necessary
+  //convertTableToMW(SINR, SINR, 10, 4);
 
   for (int i = 0; i < nConnections; i++) {
     Coordenate coord;
@@ -62,3 +71,103 @@ Model::Model(char *file) {
   //initBig_Mij();
 
 }
+
+void Model::generateInterferenceDistanceMatrix() {
+
+  Connection auxConnection;
+  Interference auxInterference;
+  double coordX1, coordY1, coordX2, coordY2, xResult, yResult, result, inteferenceSenderReceptor;
+
+  for (int i = 0; i < nConnections; i++) {
+
+    coordX1 = recCoord[i].x;
+    coordY1 = recCoord[i].y;
+
+    coordX2 = sendersCoord[i].x;
+    coordY2 = sendersCoord[i].y;
+
+
+    xResult = (coordX1 - coordX2) * (coordX1 - coordX2);
+    yResult = (coordY1 - coordY2) * (coordY1 - coordY2);
+
+    result = sqrt(xResult + yResult);
+
+    auxConnection.id = i;
+    auxConnection.distanceSenderReceptor = result;
+
+    auxConnection.powerSR = powerSender / pow(result, alfa);
+
+    auxConnection.totalInterference = 0.0;
+
+    connections.emplace_back(auxConnection);
+
+    for (int j = 0; j < nConnections; j++) {
+      if (i != j) {
+
+        auxInterference.idReceptor = i;
+        auxInterference.idSender = j;
+
+        coordX2 = sendersCoord[j].x;
+        coordY2 = recCoord[j].y;
+
+        if (coordX1 == coordX2 && coordY1 == coordY2) { //TODO: Do the comparison in the right way (with epsilon)
+          result = 0.0;
+
+          auxInterference.distanceConnections = result;
+
+          auxInterference.valueInterference = 1000000000;
+
+          interferenceMatrix[i][j] = auxInterference.valueInterference;
+
+          distanceMatrix[i][j] = result;
+
+        } else {
+
+          xResult = (coordX1 - coordX2) * (coordX1 - coordX2);
+          yResult = (coordY1 - coordY2) * (coordY1 - coordY2);
+
+          result = sqrt(xResult + yResult);
+
+
+          //Interference between the Receptor[i] and the Sender[j] (Pii/(distance_ij^alfa))
+          //OBS: See the referent constraint in the mathematical model
+          auxInterference.distanceConnections = result;
+
+          auxInterference.valueInterference = powerSender / pow(result, alfa);
+
+          interferenceMatrix[i][j] = auxInterference.valueInterference;
+
+          distanceMatrix[i][j] = result;
+
+        }
+
+      } else {
+        auxInterference.idReceptor = i;
+        auxInterference.idSender = j;
+        auxInterference.distanceConnections = auxConnection.distanceSenderReceptor;//Distance Between Sender[i] and Receptor[j]
+        auxInterference.valueInterference = 0;
+
+        interferenceMatrix[i][j] = 0.0;
+
+        distanceMatrix[i][j] = auxConnection.distanceSenderReceptor;
+      }
+    }
+  }
+
+  //TODO: Why is this necessary? Or, actually, is this necessary?
+  //recCoord.clear();
+  //sendersCoord.clear();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
